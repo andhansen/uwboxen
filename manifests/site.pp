9b7f4b46ec3c3365af3eda6d359b63322159053a
akh <andhansen@yahoo.com>
2014-06-21 22:08:43 +0200

    moved classes

--- manifests/site.pp
+++ manifests/site.pp
@@ -82,60 +82,60 @@ node default {
 
     include firefox
     include chrome
+}
 
-    class virtualbox {
-		package { 'PHPStorm':
-            provider => 'pkgdmg',
-            source   => 'http://download-cf.jetbrains.com/webide/PhpStorm-7.1.3.dmg'
-        }
+class PHPStorm {
+	package { 'PHPStorm':
+        provider => 'pkgdmg',
+        source   => 'http://download-cf.jetbrains.com/webide/PhpStorm-7.1.3.dmg'
     }
+}
 
-    class php {
-        homebrew::tap { 'josegonzalez/homebrew-php': }
+class php {
+    homebrew::tap { 'josegonzalez/homebrew-php': }
 
-        package { 'php53' :
-            ensure => present,
-            install_options => [
-                '--with-apache'
-            ]
-		}
+    package { 'php53' :
+        ensure => present,
+        install_options => [
+            '--with-apache'
+        ]
+	}
 
-		package { 'php53-memcached' :
-			ensure => present,
-			require => Package["php53"]
-		}
+	package { 'php53-memcached' :
+		ensure => present,
+		require => Package["php53"]
+	}
 
-		package { 'php53-xdebug' :
-			ensure => present,
-			require => Package['php53']
-		}
+	package { 'php53-xdebug' :
+		ensure => present,
+		require => Package['php53']
+	}
 
-		package { "php53-oauth" :
-			ensure => present,
-			require => Package['php53']
-		}
+	package { "php53-oauth" :
+		ensure => present,
+		require => Package['php53']
+	}
 
-		package { "php53-igbinary" :
-			ensure => present,
-			require => Package['php53']
-		}
+	package { "php53-igbinary" :
+		ensure => present,
+		require => Package['php53']
+	}
 
-		package { "php53-redis" :
-			ensure => present,
-			install_options => [
-		        '--with-igbinary'
-			],
-			require => Package['php53-igbinary']
-		}
+	package { "php53-redis" :
+		ensure => present,
+		install_options => [
+	        '--with-igbinary'
+		],
+		require => Package['php53-igbinary']
+	}
 
-		package { "mcrypt" :
-			ensure => present
-		}
+	package { "mcrypt" :
+		ensure => present
+	}
 
-		package { "php53-mcrypt" :
-			ensure => present,
-			require => Package['php53'],
-			require => Package['mcrypt']
-		}
-    }
+	package { "php53-mcrypt" :
+		ensure => present,
+		require => Package['php53'],
+		require => Package['mcrypt']
+	}
 }
